232dec312064d147e44be96f2bf2d3acb7e64424
knekhla <35401953+knekhla@users.noreply.github.com>
2019-02-22 15:07:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World Jerome ! This is my first Jenkins PHP200 page using OpenShift Online</p>'; ?> 
+ <?php echo '<p>Hello Candoliste</p>'; ?> 
  </body>
 </html>
